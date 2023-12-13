f1bf955911a60389375f8f37abe4337dc3cd5f53
MochNad <mochammadnadiraflimaulana@gmail.com>
2023-12-13 12:15:26 +0700

    makanan kucing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -74,7 +74,7 @@
                     <li class="menu-item">
                         <a href="javascript:void(0);" class="menu-link menu-toggle">
                             <i class="menu-icon tf-icons bx bx-star"></i>
-                            <div data-i18n="Account Settings">Jenis Kucing</div>
+                            <div data-i18n="Account Settings">Makanan Kucing</div>
                         </a>
                         <ul class="menu-sub">
                             <li class="menu-item" id="setting">
